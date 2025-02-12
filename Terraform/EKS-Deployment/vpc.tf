d05b33cbb1c88d5bf5965d1d4c18933ea5b3bcf9
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2025-02-12 13:11:25 +0530

    Update vpc.tf

--- Terraform/EKS-Deployment/vpc.tf
+++ Terraform/EKS-Deployment/vpc.tf
@@ -3,7 +3,7 @@
 resource "aws_vpc" "bankapp-eks-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-  enable_nat_gateway = true
+
   tags = {
     Name = "BankApp-EKS-VPC"
   }
